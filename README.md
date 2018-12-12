b53d1145a5a2b45c47b420508484c9361a29f7d6
Phillip Sturm <sturmphillip95@gmail.com>
2018-12-11 23:42:34 -0600

    Updated

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ We like sportz and we don't care who knows!
 By using Giphy's API, you can search for your favorite sports or athletes and watch GIF's of them!
 
 Add or delete buttons as you like and spend more time than you should distracting yourself from being productive!
+
+
+Referenced materials:
+Homeworks week 3, most of day 5.
+https://github.com/jahdasha/giphy-API
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,85 +1,95 @@
-$( document ).ready(function() {
-    var actions = ["baseball", "football", "basketball", "snowboarding", "dive", "ufc", "rock climbing", "parkour", "martial arts", "nfl", "surf"];
-  
-    function displayGifButtons(){
-        $("#gifButtonsView").empty(); 
-        for (var i = 0; i < actions.length; i++){
-            var gifButton = $("<button>");
-            gifButton.addClass("action");
-            gifButton.addClass("btn btn-primary")
-            gifButton.attr("data-name", actions[i]);
-            gifButton.text(actions[i]);
-            $("#gifButtonsView").append(gifButton);
-        }
-    }
-    
-    function addNewButton(){
-        $("#addGif").on("click", function(){
-        var action = $("#action-input").val().trim();
-        if (action == ""){
-          return false; 
-        }
-        actions.push(action);
-    
-        displayGifButtons();
-        return false;
-        });
-    }
-    
-    function removeLastButton(){
-        $("removeGif").on("click", function(){
-        actions.pop(action);
-        displayGifButtons();
-        return false;
-        });
-    }
-    
-    function displayGifs(){
-        var action = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
-        console.log(queryURL); 
+// On Page Load //
+$(document).ready(function () {
+
+    // Displaying Gifs //
+    function displaySearch() {
+        var search = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        // Getting data from the Giphy API //
         $.ajax({
             url: queryURL,
-            method: 'GET'
-        })
-        .done(function(response) {
-            console.log(response); 
-            $("#gifsView").empty();
-            var results = response.data; 
-            if (results == ""){
-              alert("There isn't a gif for this selected button");
-            }
-            for (var i=0; i<results.length; i++){
-    
-                var gifDiv = $("<div>"); 
-                gifDiv.addClass("gifDiv");
-                var gifRating = $("<p>").text("Rating: " + results[i].rating);
-                gifDiv.append(gifRating);
-                var gifImage = $("<img>");
-                gifImage.attr("src", results[i].images.fixed_height_small_still.url); 
-                gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); 
-                gifImage.attr("data-animate",results[i].images.fixed_height_small.url); 
-                gifImage.attr("data-state", "still"); 
-                gifImage.addClass("image");
-                gifDiv.append(gifImage);
-                $("#gifsView").prepend(gifDiv);
+            method: "GET",
+        }).then(function (response) {
+            $("#gifsAppearHere").empty();
+            var data = response.data;
+//Adding Classes and Displaying the GIFS on the HTML// 
+            for (var i = 0; i < data.length; i++) {
+                var newDiv = $("<div>");
+                var rated = $("<p>").text("Rated: " + data[i].rating);
+                newDiv.append(rated);
+                var gifImg = $("<img>");
+             
+                gifImg.addClass("gifs");
+                gifImg.attr("src", data[i].images.fixed_height_small_still.url);
+                gifImg.attr("data-still", data[i].images.fixed_height_small_still.url);
+                gifImg.attr("data-animate", data[i].images.fixed_height_small.url);
+                gifImg.attr("data-state", "still");
+                gifImg.addClass("image");
+                newDiv.append(gifImg);
+
+                $("#gifsAppearHere").prepend(newDiv);
+
             }
         });
+    };
+// Buttons that display on page load //
+    var initialGifs = ["baseball", "soccer", "basketball", "snowboarding", "dive", "ufc", "rock climbing", "parkour", "martial arts", "nfl", "surf"];
+
+    // Displaying the above array //
+    function gifButtons() {
+        $("#gifButtonsDisplay").empty();
+        for (let i = 0; i < initialGifs.length; i++) {
+            var gifButton = $("<button>");
+            gifButton.addClass("gifButtons btn btn-primary");
+            gifButton.attr("data-name", initialGifs[i]);
+            gifButton.text(initialGifs[i]);
+            $("#gifButtonsDisplay").append(gifButton);
+            
+        }
+    };
+
+    // My add button isn't working, I'll figure it out later //
+
+    function newBtn() {
+        $("#addGif").on("click", function () {
+            var search = $("#search-input").val().trim();
+            if (search === "") {
+                alert("Type something in the box");
+                return false; 
+            }
+            initialGifs.push(search);
+
+            displaySearch();
+            return false; 
+        });
     }
-   
-    displayGifButtons();
-    addNewButton();
-    removeLastButton();
-    $(document).on("click", ".action", displayGifs);
-    $(document).on("click", ".image", function(){
-        var state = $(this).attr('data-state');
-        if ( state == 'still'){
+
+    // Getting rid of user added buttons //
+
+    function removeBtn() {
+        $("#removeBtn").on("click", function () {
+            initialGifs.pop(search);
+            gifButtons();
+            return false;
+        });
+    }
+
+    // Allowing the user to switch between animated GIFS and still GIFS // 
+    $(document).on("click", ".gifButtons", displaySearch);
+    $(document).on("click", ".gifs", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
             $(this).attr('src', $(this).data('animate'));
             $(this).attr('data-state', 'animate');
-        }else{
+        } else {
             $(this).attr('src', $(this).data('still'));
             $(this).attr('data-state', 'still');
         }
     });
-    });
-    
+// Function Calls //
+    removeBtn();
+    newBtn();
+    gifButtons();
+    displaySearch();
+});  
